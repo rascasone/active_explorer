1c724f075f4b18a4d5dfb0d438521725a223024c
Marek Ulicny <xulicny@gmail.com>
2016-04-05 21:46:57 +0200

    Refactor - create Mindmap class.
    It serves wel for generating graph. No need to transfer so many variables anymore - they are switched to instance varibales.

--- lib/mindmapper.rb
+++ lib/mindmapper.rb
@@ -3,58 +3,85 @@ require "mindmapper/version"
 require 'graphviz'
 
 module Mindmapper
-  def generate_mindmap(file_path:, associations: [])
-    raise TypeError, "Parameter 'associations' must be Array but is #{associations.class}." unless associations.is_a? Array
+  def generate_mindmap(file_path:, associations_filter: [], max_depth: 3)
+    raise TypeError, "Parameter 'associations_filter' must be Array but is #{associations_filter.class}." unless associations_filter.is_a? Array
 
-    filename = file_path.split(File::SEPARATOR).last
-    directory = file_path.chomp filename
-
-    create_directory directory
-
-    generete_graph file_path, associations
+    mindmap = Mindmap.new self, max_depth, associations_filter
+    mindmap.save_to_file file_path
   end
 
   private
 
-  def create_directory(directory)
-    unless File.directory? directory
-      FileUtils.mkdir_p directory
-    end
-  end
+  class Mindmap
+    def initialize(object, max_depth, associations_filter)
+      raise TypeError, "Parameter 'associations_filter' must be Array but is #{associations_filter.class}." unless associations_filter.is_a? Array
 
-  def generete_graph(file_path, associations = [])
-    raise TypeError, "Parameter 'associations' must be Array but is #{associations.class}." unless associations.is_a? Array
+      @object = object
+      @max_depth = max_depth
+      @associations = get_associtations(associations_filter)
 
-    g = GraphViz.new(:G, :type => :digraph)
-
-    main_class_name = self.class.name
-    main_attributes = self.attributes.keys.join("\n")
-    main_values = self.attributes.values.join("\n")
-
-    main_node = g.add_nodes("main_class", shape: "record", label: "{<f0> #{main_class_name}|{<f1> #{main_attributes}|<f2> #{main_values}}}")
-
-    associations = associations.collect { |association| association.to_s }
-
-    filtered_associations = self.class.reflections.collect do |association|
-      (associations.include? association.first) ? association.second : nil
+      generate_graph
     end
 
-    filtered_associations.compact!
+    def save_to_file(file_path)
+      filename = file_path.split(File::SEPARATOR).last
+      directory = file_path.chomp filename
 
-    filtered_associations.each do |association|
-      models = self.send(association.name)
+      create_directory directory
 
-      models.each do |model|
-        model_class_name = model.class.name
-        model_attributes = model.attributes.keys.join("\n")
-        model_values = model.attributes.values.join("\n")
+      @graph.output(:png => file_path)
+    end
 
-        model_node = g.add_nodes("#{model.class.name}_#{model.id}", shape: "record", label: "{<f0> #{model_class_name}|{<f1> #{model_attributes}|<f2> #{model_values}}}")
+    private
 
-        g.add_edges(main_node, model_node)
+    def create_directory(directory)
+      unless File.directory? directory
+        FileUtils.mkdir_p directory
       end
     end
 
-    g.output(:png => file_path)
+    def get_associtations(associations_filter)
+      associations_filter.collect! { |association| association.to_s }
+
+      associations = @object.class.reflections.collect do |reflection|
+        reflection.second
+      end
+
+      associations.select! do |association|
+        associations_filter.include? association.name.to_s
+      end
+
+      associations
+    end
+
+    # Empty
+    # def generate_graph(file_path, current_depth = 0, max_depth = 3, associations = [] )
+    def generate_graph
+      @graph = GraphViz.new(:G, :type => :digraph)
+
+      main_class_name = @object.class.name
+      main_attributes = @object.attributes.keys.join("\n")
+      main_values = @object.attributes.values.join("\n")
+
+      @main_node = @graph.add_nodes("main_class", shape: "record", label: "{<f0> #{main_class_name}|{<f1> #{main_attributes}|<f2> #{main_values}}}")
+
+      add_subnodes
+    end
+
+    def add_subnodes
+      @associations.each do |association|
+        models = @object.send(association.name)
+
+        models.each do |model|
+          model_class_name = model.class.name
+          model_attributes = model.attributes.keys.join("\n")
+          model_values = model.attributes.values.join("\n")
+
+          model_node = @graph.add_nodes("#{model.class.name}_#{model.id}", shape: "record", label: "{<f0> #{model_class_name}|{<f1> #{model_attributes}|<f2> #{model_values}}}")
+
+          @graph.add_edges(@main_node, model_node)
+        end
+      end
+    end
   end
 end
